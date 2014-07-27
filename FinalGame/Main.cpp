07627c0fc947aaf3e9dceb2c8ae88d53246ea981
Caleb <crazeddmofo@gmail.com>
2014-07-27 14:34:57 -0500

    Can store up to three keys in inventory. doors only open with specific key. made functions to handle opening doors and locked doors, function to sotre keys didnt work. don't know why

--- FinalGame/Main.cpp
+++ FinalGame/Main.cpp
@@ -6,11 +6,15 @@
 #define MAP_HEIGHT 31
 #include <iostream>
 #include <string>
+#include <istream>
 #include <conio.h>
 #include "Player.h"
+
 void DrawMap(char Map[MAP_WIDTH][MAP_HEIGHT]);
 bool Collision(char FutureLocation, char Barrier);
-
+bool Collision(char FutureLocation, char Door[]);
+void GetKey(char FutureLocation, Player Player1, char Key[]);
+bool OpenDoor(char FutureLocation, Player Player1, char Door[], char Key[]);
 
 using namespace std;
 
@@ -24,8 +28,8 @@ void main()
 	char Input;
 	char FutureLocation; //To store players next location in the Map
 	char Wall = '#'; //Wall that player cannot pass
-	char Door = 'D';
-	char Key = 'd';
+	char Door[3]= {'D','H','T'};
+	char Key[3] = {'d', 'h', 't'};
 
 	Player Player1;
 	Player1.Name = '@';
@@ -49,9 +53,9 @@ void main()
 		{'#',' ','#','#','#',' ','#',' ','#',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#',' ',' ',' ',' ',' ','#',' ','#','#',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#',' ','#','#','#','#','#','#','#','#',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
-		{'#',' ',' ',' ','#',' ',' ',' ',' ','#',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+		{'#',' ',' ',' ','#',' ',' ',' ',' ','#',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',Key[0],' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#',' ',' ',' ','#',' ',' ','#',' ','#',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
-		{'#',' ','#',' ','#',' ',' ','#',' ','#','#','#','#','#','#','D','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+		{'#',Door[1],'#',' ','#',' ',' ','#',' ','#','#','#','#','#','#',Door[0],'#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#',' ','#',' ','#',' ',' ','#',' ','#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#',' ','#',' ','#','#',' ','#',' ','#',' ','#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#',' ','#',' ',' ',' ',' ','#',' ',' ',' ','#',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
@@ -62,7 +66,7 @@ void main()
 		{'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#','#',' ','#','#','#','#',' ','#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#','#',' ','#',' ',' ','#','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
-		{'#','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+		{'#','#',' ',' ',' ',' ',Key[1],'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
 		{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}
 	};
 
@@ -77,6 +81,7 @@ void main()
 	while (true)
 	{
 		Input = getch();
+		
 
 		//get keystroke and do thing that is supposed to be done
 //Switch Statement
@@ -86,71 +91,138 @@ void main()
 				FutureLocation = MapWithPlayer[Player1.y][Player1.x + 1];
 
 				//if Future location has a key, store key in inventory
-				if(FutureLocation == Key)
-				{	
-					
-					Player1.Inventory[0] = Key;
+				//GetKey(FutureLocation, Player1, Key);
+				if(FutureLocation == Key[0])
+				{						
+					Player1.Inventory[0] = Key[0];
 				}
-
-				//if Future location has some cort of barrier then player can not move forward
+				if(FutureLocation == Key[1])
+				{						
+					Player1.Inventory[1] = Key[1];
+					//Key[1] =' ';
+				}
+				if(FutureLocation == Key[2])
+				{						
+					Player1.Inventory[2] = Key[2];
+					//Key[2] =' ';
+				}
+				
+				//if Future location has some sort of barrier then player can not move forward
 				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.x < MAP_WIDTH - 1)
 				{				
 						Player1.Right();
 				}
 				//if future location is a door and player has key in inventory player can move through door.
-				if(FutureLocation == Door && Player1.Inventory[0] == Key)
+				if(OpenDoor(FutureLocation, Player1, Door, Key))
 				{
 					Player1.Right();
-				}				
+				}
+				//if Futurelocation is a door and player doesnt have a key, display message.
+				if(OpenDoor(FutureLocation, Player1, Door, Key))
+				{
+					cout << "This Door is locked! You need a key!" << endl;
+					system("PAUSE>NULL");
+				}
 				break;	
 
 			case LEFT_ARROW:
 				FutureLocation = MapWithPlayer[Player1.y][Player1.x - 1];
-				if(FutureLocation == Key)
-				{					
-					Player1.Inventory[0] = Key;
+				//GetKey(FutureLocation, Player1, Key);
+				if(FutureLocation == Key[0])
+				{						
+					Player1.Inventory[0] = Key[0];
 				}
+				if(FutureLocation == Key[1])
+				{						
+					Player1.Inventory[1] = Key[1];
+					//Key[1] =' ';
+				}
+				if(FutureLocation == Key[2])
+				{						
+					Player1.Inventory[2] = Key[2];
+					//Key[2] =' ';
+				}
+
 				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.x > 0)
 				{
 					Player1.Left();
 				}
-				if(FutureLocation == Door && Player1.Inventory[0] == Key)
+				if(OpenDoor(FutureLocation, Player1, Door, Key))
 				{
 					Player1.Left();
+				}
+				if(OpenDoor(FutureLocation, Player1, Door, Key))
+				{
+					cout << "This Door is locked! You need a key!" << endl;
+					system("PAUSE>NULL");
 				}
 				break;	
 
 			case DOWN_ARROW:
 				FutureLocation = MapWithPlayer[Player1.y + 1][Player1.x];
-				if(FutureLocation == Key)
-				{					
-					Player1.Inventory[0] = Key;
+
+				//GetKey(FutureLocation, Player1, Key);
+				if(FutureLocation == Key[0])
+				{						
+					Player1.Inventory[0] = Key[0];
 				}
+				if(FutureLocation == Key[1])
+				{						
+					Player1.Inventory[1] = Key[1];
+					//Key[1] =' ';
+				}
+				if(FutureLocation == Key[2])
+				{						
+					Player1.Inventory[2] = Key[2];
+					//Key[2] =' ';
+				}
+
 				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.y < MAP_HEIGHT - 1)
 				{
 					Player1.Down();
 				}
-				if(FutureLocation == Door && Player1.Inventory[0] == Key)
+				if (OpenDoor(FutureLocation, Player1, Door, Key))
 				{
 					Player1.Down();
+				}
+				if(OpenDoor(FutureLocation, Player1, Door, Key))
+				{
+					cout << "This Door is locked! You need a key!" << endl;
+					system("PAUSE>NULL");
 				}
 				break;
 
 			case UP_ARROW:
 				FutureLocation = MapWithPlayer[Player1.y - 1][Player1.x];
+
 				//if Future location has a key, store key in inventory
-				if(FutureLocation == Key)
-				{					
-					Player1.Inventory[0] = Key;
+				//GetKey(FutureLocation, Player1, Key);
+				if(FutureLocation == Key[0])
+				{						
+					Player1.Inventory[0] = Key[0];
 				}
-				
+				if(FutureLocation == Key[1])
+				{						
+					Player1.Inventory[1] = Key[1];
+					//Key[1] =' ';
+				}
+				if(FutureLocation == Key[2])
+				{						
+					Player1.Inventory[2] = Key[2];
+					//Key[2] =' ';
+				}
 				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.y > 0)
 				{
 					Player1.Up();
 				}
-				if(FutureLocation == Door && Player1.Inventory[0] == Key)
+				if (OpenDoor(FutureLocation, Player1, Door, Key))
 				{
 					Player1.Up();
+				}
+				if(OpenDoor(FutureLocation, Player1, Door, Key))
+				{
+					cout << "This Door is locked! You need a key!" << endl;
+					system("PAUSE>NULL");
 				}
 				break;
 		}
@@ -203,5 +275,51 @@ bool Collision(char FutureLocation, char Barrier)
 		return true;
 	}
 }
+bool Collision(char FutureLocation, char Door[])
+{
+	if(FutureLocation == Door[0] || FutureLocation == Door[1] || FutureLocation == Door[2])
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+void GetKey(char FutureLocation, Player Player1, char Key[])
+{
+	if(FutureLocation == Key[0])
+	{						
+		Player1.Inventory[0] = Key[0];
+	}
+	if(FutureLocation == Key[1])
+	{						
+		Player1.Inventory[1] = Key[1];
+		//Key[1] =' ';
+	}
+	if(FutureLocation == Key[2])
+	{						
+		Player1.Inventory[2] = Key[2];
+		//Key[2] =' ';
+	}
+}
+bool OpenDoor(char FutureLocation, Player Player1, char Door[], char Key[])
+{
+	if(FutureLocation == Door[0] && Player1.Inventory[0] == Key[0])
+	{
+		return true;
+	}
+	else if(FutureLocation == Door[1] && Player1.Inventory[1] == Key[1])
+	{
+		return true;
+	}
+	else if(FutureLocation == Door[2] && Player1.Inventory[2] == Key[2])
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 
-	
