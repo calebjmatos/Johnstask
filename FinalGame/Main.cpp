986b379fceba38147d7119d2fd3dfb2a41a0c592
Caleb <crazeddmofo@gmail.com>
2014-07-31 21:24:23 -0500

    Added Win Screen, and replay option

--- FinalGame/Main.cpp
+++ FinalGame/Main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 #include <conio.h>
+#include <stdio.h>
 #include <fstream>
 #include "Player.h"
 
@@ -16,6 +17,7 @@ bool Collision(char FutureLocation, char Barrier);
 bool Collision(char FutureLocation, char Door[]);
 void GetKey(char FutureLocation, Player Player1, char Key[]);
 bool OpenDoor(char FutureLocation, Player Player1, char Door[], char Key[]);
+bool TryAgain();
 
 using namespace std;
 
@@ -34,6 +36,8 @@ void main()
 	char Key[3] = {'d', 'h', 't'};
 	ifstream inFile; //to read title screen and maps
 	char Title[6][53];
+	bool PlayAgain = true;
+	bool RepeatMainLoop = true;
 
 	Player Player1; //Player object
 	Player1.Name = '@';
@@ -82,6 +86,7 @@ void main()
 	Input = getch();
 	system("CLS");
 
+
 	cout << "The King Charged John with the task of finding the Stone and saving the kingdom, \n"
 			"                        This is" << endl;
 	inFile.open("Title.txt");
@@ -106,6 +111,9 @@ void main()
 		cout << endl;
 	}
 	inFile.close();
+	cout << "\n\n Created by: Caleb Matos \n\n"
+		"WARNING! MAY CAUSE SEZIURES!......Lol, jk.......but seriously." << endl;
+	
 	Input = getch();
 	system("CLS");
 	
@@ -113,7 +121,8 @@ void main()
 							/*________End Story and Splash Screen_________*/
 	/*___________________________________________________________________________________________*/
 
-	
+do
+{
 //Map, Open Map File and read into Mapp array
 	char Map[14][78];
 	inFile.open("Map.txt");
@@ -153,56 +162,75 @@ void main()
 	/*___________________________________________________________________________________________*/
 					/*________________Main Game Loop_______________________*/
 	/*___________________________________________________________________________________________*/
-	cout << "\nNavigate the Labyrinth and collect the Keys to open the doors." << endl;
-
-	while (true)
-	{
-		
-		Input = getch();
-
-		
-
-		//get keystroke and do thing that is supposed to be done
-		//Switch Statement
-		switch(Input)
+	cout << "\nUse the arrow keys to navigate the Labyrinth. Collect all the Keys to open the \ndoors." << endl;
+	RepeatMainLoop = true;
+		while (RepeatMainLoop)
 		{
-			case RIGHT_ARROW:
-				FutureLocation = MapWithPlayer[Player1.y][Player1.x + 1];
+		
+			Input = getch();
 
-				//if Future location has a key, store key in inventory
-				if(FutureLocation == Key[0])
-				{						
-					Player1.Inventory[0] = Key[0];
-					Map[Player1.y][Player1.x + 1] = ' ';
-				}
-				if(FutureLocation == Key[1])
-				{						
-					Player1.Inventory[1] = Key[1];
-					Map[Player1.y][Player1.x + 1] = ' ';
-				}
-				if(FutureLocation == Key[2])
-				{						
-					Player1.Inventory[2] = Key[2];
-					Map[Player1.y][Player1.x + 1] = ' ';
-				}
-				
-				//if Future location has some sort of barrier then player can not move forward
-				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.x < MAP_HEIGHT - 1)
-				{				
-					Player1.Right();
-				}
-				//if future location is a door and player has key in inventory player can move through door.
-				if(OpenDoor(FutureLocation, Player1, Door, Key))
-				{
-					Player1.Right();
-					Map[Player1.y][Player1.x + 1] = ' ';
-				}
-				
-				
-				break;	
+		
 
-			case LEFT_ARROW:
-				FutureLocation = MapWithPlayer[Player1.y][Player1.x - 1];
+			//get keystroke and do thing that is supposed to be done
+			//Switch Statement
+			switch(Input)
+			{
+				case RIGHT_ARROW:
+					FutureLocation = MapWithPlayer[Player1.y][Player1.x + 1];
+
+					//if Future location has a key, store key in inventory
+					if(FutureLocation == Key[0])
+					{						
+						Player1.Inventory[0] = Key[0];
+						Map[Player1.y][Player1.x + 1] = ' ';
+					}
+					if(FutureLocation == Key[1])
+					{						
+						Player1.Inventory[1] = Key[1];
+						Map[Player1.y][Player1.x + 1] = ' ';
+					}
+					if(FutureLocation == Key[2])
+					{						
+						Player1.Inventory[2] = Key[2];
+						Map[Player1.y][Player1.x + 1] = ' ';
+					}
+					if(FutureLocation == '$')
+					{
+						system("CLS");
+						cout << "HUZZAH! You found the ancient healing stone and saved the kingdom!" << endl;
+						RepeatMainLoop = TryAgain();
+						if (RepeatMainLoop == false)
+						{							
+							PlayAgain = false;
+						}
+						else
+						{
+							system("CLS");
+							Player1.x = 0;
+							Player1.y = 1;
+							PlayAgain = true;
+						}
+						
+							
+					}
+									
+					//if Future location has some sort of barrier then player can not move forward
+					if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.x < MAP_HEIGHT - 1)
+					{				
+						Player1.Right();
+					}
+					//if future location is a door and player has key in inventory player can move through door.
+					if(OpenDoor(FutureLocation, Player1, Door, Key))
+					{
+						Player1.Right();
+						Map[Player1.y][Player1.x + 1] = ' ';
+					}
+				
+				
+					break;	
+
+				case LEFT_ARROW:
+																																	FutureLocation = MapWithPlayer[Player1.y][Player1.x - 1];
 				//GetKey(FutureLocation, Player1, Key);
 				if(FutureLocation == Key[0])
 				{						
@@ -221,7 +249,25 @@ void main()
 					Map[Player1.y][Player1.x - 1] = ' ';	
 				}
 				
-
+				if(FutureLocation == '$')
+					{
+						system("CLS");
+						cout << "HUZZAH! You found the ancient healing stone and saved the kingdom!" << endl;
+						RepeatMainLoop = TryAgain();
+						if (RepeatMainLoop == false)
+						{							
+							PlayAgain = false;
+						}
+						else
+						{
+							system("CLS");
+							Player1.x = 0;
+							Player1.y = 1;
+							PlayAgain = true;
+						}
+						
+							
+					}
 				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.x > 0)
 				{
 					Player1.Left();
@@ -234,8 +280,8 @@ void main()
 				
 				break;	
 
-			case DOWN_ARROW:
-				FutureLocation = MapWithPlayer[Player1.y + 1][Player1.x];
+				case DOWN_ARROW:
+																																	FutureLocation = MapWithPlayer[Player1.y + 1][Player1.x];
 
 				//GetKey(FutureLocation, Player1, Key);
 				if(FutureLocation == Key[0])
@@ -253,7 +299,25 @@ void main()
 					Player1.Inventory[2] = Key[2];
 					Map[Player1.y + 1][Player1.x] =' ';
 				}
-				
+				if(FutureLocation == '$')
+					{
+						system("CLS");
+						cout << "HUZZAH! You found the ancient healing stone and saved the kingdom!" << endl;
+						RepeatMainLoop = TryAgain();
+						if (RepeatMainLoop == false)
+						{							
+							PlayAgain = false;
+						}
+						else
+						{
+							system("CLS");
+							Player1.x = 0;
+							Player1.y = 1;
+							PlayAgain = true;
+						}
+						
+							
+					}
 				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.y < MAP_HEIGHT - 1)
 				{
 					Player1.Down();
@@ -266,8 +330,8 @@ void main()
 				
 				break;
 
-			case UP_ARROW:
-				FutureLocation = MapWithPlayer[Player1.y - 1][Player1.x];
+				case UP_ARROW:
+																																					FutureLocation = MapWithPlayer[Player1.y - 1][Player1.x];
 
 				//if Future location has a key, store key in inventory
 				//GetKey(FutureLocation, Player1, Key);
@@ -287,10 +351,24 @@ void main()
 					Map[Player1.y - 1][Player1.x] =' ';
 				}
 				if(FutureLocation == '$')
-				{
-					break;
-				
-				}
+					{
+						system("CLS");
+						cout << "HUZZAH! You found the ancient healing stone and saved the kingdom!" << endl;
+						RepeatMainLoop = TryAgain();
+						if (RepeatMainLoop == false)
+						{							
+							PlayAgain = false;
+						}
+						else
+						{
+							system("CLS");
+							Player1.x = 0;
+							Player1.y = 1;
+							PlayAgain = true;
+						}
+						
+							
+					}
 				if(Collision(FutureLocation, Wall) && Collision(FutureLocation, Door) && Player1.y > 0)
 				{
 					Player1.Up();
@@ -301,34 +379,32 @@ void main()
 					Map[Player1.y - 1][Player1.x] = ' ';
 				}
 				break;
-		}
-	
-		
-		
-
-		//copy new maze with player location into old maze
-		for(int y = 0; y <= MAP_WIDTH - 1; y++)
-		{
-			for (int x = 0; x <= MAP_HEIGHT - 1; x++)
-			{
-				MapWithPlayer[y][x] = Map[y][x];
 			}
+		
+			//copy new maze with player location into old maze
+			for(int y = 0; y <= MAP_WIDTH - 1; y++)
+			{
+				for (int x = 0; x <= MAP_HEIGHT - 1; x++)
+				{
+					MapWithPlayer[y][x] = Map[y][x];
+				}
+			}
+
+			//update player location 
+			MapWithPlayer[Player1.y][Player1.x] = Player1.Name;
+
+			//clear screen
+			system("CLS");
+			//redraw with players new Location
+			DrawMap(MapWithPlayer);
+		}	/*___________________________________________________________________________________________*/
+								/*_______________End Main Game Loop_______________________*/
+			/*___________________________________________________________________________________________*/
 		}
+		while(PlayAgain);
 
-		//update player location 
-		MapWithPlayer[Player1.y][Player1.x] = Player1.Name;
+		cout << "\nTHANK YOU FOR PLAYING!\n" << endl;
 
-		//clear screen
-		system("CLS");
-		//redraw with players new Location
-		DrawMap(MapWithPlayer);
-	}
-	
-	/*___________________________________________________________________________________________*/
-					/*_______________End Main Game Loop_______________________*/
-	/*___________________________________________________________________________________________*/
-	
-	
 }//end void main
 				
 
@@ -402,4 +478,29 @@ bool OpenDoor(char FutureLocation, Player Player1, char Door[], char Key[])
 	{
 		return false;
 	}
+}
+bool TryAgain ()
+{
+	
+	char cAnswer;
+
+	cout << "\nWould you like to Play Again? y/n:  \n";
+	cin >> cAnswer;
+	cAnswer = toupper(cAnswer);
+	
+	if (cAnswer == 'N')
+	{
+		return false;
+	}
+	else if (cAnswer == 'Y')
+	{
+		return true;
+	}
+	else 
+	{
+		cout << "Invalid Responseb \n " << endl;
+		return false;
+	}
+	
+
 }
